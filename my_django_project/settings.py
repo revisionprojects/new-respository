05faecdb3fa223d70a9a982e83c27a68c455de17
revisionprojects <robinmatth@live.ca>
2021-09-04 18:24:27 -0400

    attempt to get database working

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'o7m6r49i+_^4b#eb5z7wipj9+s9gm(ao&po5hqcgzv*tie0^4z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://revision-projects.herokuapp.com/']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Stock'
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -51,8 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'my_django_project.urls'
 
 TEMPLATES = [
@@ -75,15 +77,23 @@ WSGI_APPLICATION = 'my_django_project.wsgi.application'
 
 
 # Database
+bashCommand = “heroku config:get DATABASE_URL -a app_name” #Use your app_name
+
+output = subprocess.check_output([‘bash’,’-c’, bashCommand]).decode(“utf-8”) # executing the bash command and converting byte to string
+
+DATABASES[‘default’] = dj_database_url.config(default=output,conn_max_age=600, ssl_require=True) #making connection to heroku DB without having to set DATABASE_URL env variable
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+#}
 
+
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 #DATABASE_URL = os.environ['postgres://wlxvjqcfgjjjfp:f455971a1045b90ba66e67ae66af7163c667eee5e52b6da67b52cf9b32d4230f@ec2-54-156-60-12.compute-1.amazonaws.com:5432/de3u5bvtb0u0a2']
 
 #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -125,6 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
