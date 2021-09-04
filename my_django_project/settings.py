070118b9589376fd3e3b179b2f3ae89931761018
revisionprojects <robinmatth@live.ca>
2021-09-04 19:22:27 -0400

    added postgres connection string for production only

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +77,15 @@ WSGI_APPLICATION = 'my_django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+DATABASE_URL = os.environ['postgres://wlxvjqcfgjjjfp:f455971a1045b90ba66e67ae66af7163c667eee5e52b6da67b52cf9b32d4230f@ec2-54-156-60-12.compute-1.amazonaws.com:5432/de3u5bvtb0u0a2']
 
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
